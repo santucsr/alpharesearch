6d6ed0f75a42df2015f25a8ba413d2bd47a66942
santucsr <santucsr@gmail.com>
2023-05-11 10:54:23 -0400

    git commit -am "week 6"

--- src/week5-test-alphas2.py
+++ src/week5-test-alphas2.py
@@ -7,10 +7,10 @@ warnings.filterwarnings('ignore')
 if __name__ == "__main__":
     # plots and daily pnl results will be saved in the plot folder
     cfg_list = []
-    for delay in [20]:
-        for a in [0.5]:#, 2, 3, 5, 7, 10]:
+    for delay in [3, 5, 10, 20, 60]:
+        for a in [0.1, 0.3, 0.5, 0.7]:#, 2, 3, 5, 7, 10]:
 
-            cfg_list.append(('YaoReV015', {'start_date': '20180101',
+            cfg_list.append(('YaoReV009', {'start_date': '20180101',
                                         'end_date': '20201231',
                                         'window': delay,
                                         'a': a,
@@ -20,11 +20,11 @@ if __name__ == "__main__":
     alpha_calc.calc(cfg_list)
  
     args_dict_list = []
-    for delay in [20]:#, 5, 10, 20, 60, 120]:
-        for a in [0.5]:
+    for delay in [3, 5, 10, 20, 60]:
+        for a in [0.1, 0.3, 0.5, 0.7]:  # , 2, 3, 5, 7, 10]:
             for holdings in [1, 3, 5, 10, 20, 60]:
                 args_dict_list.append({
-                    'table_name': f'alpha.YaoReV015-{delay}days-a{int(a*10)}-zz9999',
+                    'table_name': f'alpha.YaoReV009-{delay}days-a{int(a*10)}-zz9999',
                     # 'table_name': f'alpha.YaoReV014-{delay}days-zz9999',
                     'start_date': '20180101',
                     'end_date': '20201231',
